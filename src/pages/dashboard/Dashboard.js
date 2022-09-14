c10651e516d4db934f11073c5eaf732c04a33a3a
Arjun <arjun2000raj@gmail.com>
2022-09-15 02:33:10 +0530

    dashbord created

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { DashboardHeader } from '../../component/DashboardHeader'
-
+import { useDispatch, useSelector } from "react-redux";
+import { getAllUsers } from '../../redux/slices/userSlice';
+import Pagination from '@mui/material/Pagination';
+import './dashboard.css'
 export const Dashboard = () => {
+  const dispatch = useDispatch()
+  const { users } = useSelector(state => state.user)
+  useEffect(() => {
+    dispatch(getAllUsers())
+  }, [])
+
   return (<>
     <DashboardHeader />
-    <div>Dashboard</div>
+    <div className='dashboard'>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>firstName</th>
+          <th>lastName</th>
+          <th>email</th>
+          <th>Photo</th>
+        </tr>
+        <tr>
+          {
+            users?.map(user => {
+              return<>
+              <th>{user.id}</th>
+              <th>{user.firstName}</th>
+              <th>{user.lastName}</th>
+              <th>{user.email}</th>
+              <th><img src={user.avatar} /></th>
+              </> 
+            })
+          }
+        </tr>
+      </table>
+    </div>
+    <div spacing={2}>
+      <Pagination count={10} color="primary" />
+    </div>
   </>
   )
 }
--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -1,14 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios"
+export const getAllUsers =  createAsyncThunk("user/getAllUsers", async() => {
+    try{
+        const {data} = await axios.get("https://reqres.in/api/users?page=2")
+        return data;
+    }catch(err){
 
+    }
+})
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
         users: [],
-        loggedInUser: null,
         error: null,
+        totalPages: 1,
+        currentPage: 1,
+        total: 0
+
     },
     extraReducers: {
+        [getAllUsers.pending]: (state, action) => {
 
+        },
+        [getAllUsers.fulfilled]: (state, action) => {
+            state.users = action.payload.data;
+            state.totalPages = action.payload.total_pages;
+            state.currentPage = action.payload.page;
+            state.total = action.payload.total;
+        },
+        [getAllUsers.rejected]: (state, action) => {
+
+        },
     }
 })
 
